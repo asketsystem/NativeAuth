b57c17facaddbb95c1b91fb48a2d8784d7a8e740
asketsystem <asketsystem1@gmail.com>
2022-02-12 19:45:25 +0000

    Custom Username & Password Input

--- src/screens/SignInSreen/SignInScreen.js
+++ src/screens/SignInSreen/SignInScreen.js
@@ -1,8 +1,12 @@
 import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../../../assets/images/Logo_1.png';
+import CustomInput from '../../components/CustomInput';
 
 const SignInScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const {height} = useWindowDimensions();
 
   return (
@@ -11,6 +15,18 @@ const SignInScreen = () => {
         source={Logo}
         style={[styles.logo, {height: height * 0.3}]}
         resizeMode="contain"
+      />
+
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry
       />
     </View>
   );

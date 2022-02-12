34c1fafa0ac51ed65029710a9f88e0933a468c94
asketsystem <asketsystem1@gmail.com>
2022-02-12 19:16:59 +0000

    Sign IN Screen - Logo

--- src/screens/SignInSreen/SignInScreen.js
+++ src/screens/SignInSreen/SignInScreen.js
@@ -1,12 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
 import React from 'react';
+import Logo from '../../../assets/images/Logo_1.png';
 
 const SignInScreen = () => {
+  const {height} = useWindowDimensions();
+
   return (
-    <View>
-      <Text>Sign In Screen</Text>
+    <View style={styles.root}>
+      <Image
+        source={Logo}
+        style={[styles.logo, {height: height * 0.3}]}
+        resizeMode="contain"
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  },
+});
 
 export default SignInScreen;
